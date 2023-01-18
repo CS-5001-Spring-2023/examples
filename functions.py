b2abd6b6ea6b2979f4f049752002f2769aab2acd
Sami Rollins <srollins@gmail.com>
2023-01-18 17:00:23 -0500

    update functions examples

--- functions.py
+++ functions.py
@@ -1,9 +1,16 @@
 
+
+def welcome():
 	"""
 	Exercise 1:
 	A function to print a multi-line welcome message.
+	# example of how to print this documentation - print(welcome.__doc__)
 	"""
+	print("Hello lovely class.")
+	print("Welcome to CS 5001")
+	print("on this fine winter day.")
 
+def greet_by_name(name):
 	"""
 	Exercise 2:
 	A function to greet a user by name.
@@ -11,7 +18,9 @@
 	name : str
 		name of the user
 	"""
+	print(f"Hello, {name}!")
 
+def get_greeting():
 	"""
 	Exercise 3:
 	A function to get a specific greeting from the user.
@@ -19,7 +28,11 @@
 	str
 		name entered by the user
 	"""
+	greeting = input("How would you like to greet your user? ")
+	return greeting
 
+
+def welcome_by_name_with_greeting(name, greeting):
 	"""
 	Exercise 4:
 	A function to welcome a user by name with a specific
@@ -30,3 +43,23 @@
 	greeting : str
 		greeting to use 	
 	"""
+	print(f"{greeting}, {name}")
+
+def main():
+	name = "Liz"
+	greeting = get_greeting()
+	welcome_by_name_with_greeting(name, greeting)
+
+	name = "Ethan"
+	welcome_by_name_with_greeting(name, get_greeting())
+
+	name = "Timothy"
+	welcome_by_name_with_greeting(name, greeting)
+
+
+main()
+
+
+
+
+
--- guessing_game.py
+++ guessing_game.py
@@ -8,5 +8,68 @@ message will be displayed.
 If the user guesses incorrectly, a "Try again!" 
 message will be displayed.
 """
+from random import randint
 
+
+# function takes no input parameters
+def get_computer_choice():
+	
+	# generate a random number
+	value = randint(1, 10)
+	
+	# return that random number
+	return value
+
+# function takes no input parameters
+def get_user_choice():
+	
+	# get choice from user
+	value = int(input("What is your choice? (number 1-10) - "))
+
+	# return the user's choice
+	return value
+	
+
+#function takes two parameters
+# -- user_choice and computer_choice
+def user_wins(user_choice, computer_choice):
+	# returns true if numbers are the same and false otherwise
+	
+	if(user_choice == computer_choice):
+		return True
+	else:
+		return False
+
+	# elif(user_choice != computer_choice):
+	# return (user_choice == computer_choice)
+
+# function takes two inputs
+# -- user_choice, computer_choice
+def declare_winner(user_choice, computer_choice):
+	# prints the final result
+	result = user_wins(user_choice, computer_choice)
+	if(result):
+		print("User has won!")
+	else:
+		print("Computer has won")
+
+
+def main():
+	# Computer chooses a number
+	computer_choice = get_computer_choice()
+	print(f"Computer chose {computer_choice}")
+
+	# Ask user for number
+	user_choice = get_user_choice()
+	print(f"You chose {user_choice}")
+
+	# # Compare numbers
+	# result = user_wins(user_choice, computer_choice)
+	# decided to change design -- compare numbers from 
+	# within declare_winner function
+
+	# Declare win/loss
+	declare_winner(user_choice, computer_choice)
+
+main()
 
