c18f483dfe04c3a1546d12f3849b64c591988ad0
Sami Rollins <srollins@gmail.com>
2023-01-25 12:25:32 -0500

    add password_checker example

---
+++ password_checker.py
@@ -0,0 +1,10 @@
+"""
+Write a program that will prompt a user for a password and verify that it matches a pre-set secret password. As long as the password entered is incorrect prompt the user to re-enter the password.
+
+Modify your solution so that the user can only try to enter the correct password a maximum of three times.
+"""
+ACTUAL_PASSWORD = "5001ROCKs"
+
+def main():
+
+main()
