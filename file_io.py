824321239265c5e8c7c84d6ee62f8aff3e68728a
Sami Rollins <srollins@gmail.com>
2023-02-09 17:06:13 -0500

    add recursion and file i/o examples

---
+++ file_io.py
@@ -0,0 +1,28 @@
+import sys
+
+def search_replace(search, replace, infilename, outfilename):
+	"""
+	Write a function that will replace all instances of a search
+	term in a given file with a replace term. The result will be 
+	saved to a new file.
+	Parameters:
+	search : str
+		the string to replace
+	replace : str
+		the replacement string
+	infilename : str
+		the name of the original file
+	outfilename : str
+		the name of the file where the result will be saved
+	"""
+
+def main():
+	# The search term, replace term, input file name, and output file name
+	# will be passed as command line parameters.
+	if len(sys.argv) != 5:
+		print("usage: python file_input.py <search> <replace> <infilename> <outfilename")
+		exit()
+	search_replace(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+
+main()
---
+++ recursion.py
@@ -0,0 +1,59 @@
+def factorial_iterative(n):
+	"""
+	Implement a function `def factorial(n):` in Python. The function takes as input an
+	integer and returns the factorial of that number.
+	"""
+	pass
+
+def factorial_recursive(n):
+	"""
+	Implement a function `def factorial(n):` in Python. The function takes as input an
+	integer and returns the factorial of that number.
+	Do not use a loop in the implementation of this function.
+	"""	
+	pass
+
+def print_string(string):
+	"""
+	Implement a recursive function that takes as input a str and prints 
+	the characters of the str one per line *without using a loop*. 
+	"""
+	pass
+
+def print_string_backward(string):
+	"""
+	Implement a recursive function that takes as input a str and prints 
+	the characters of the str *in reverse* one per line *without using a loop*. 
+	"""
+	pass	
+
+def print_nums_iterative(n):
+	"""
+	A function that takes as input a number and prints from 1 to 
+	the number (inclusive).
+	"""
+	pass
+
+def print_nums_recursive(n):
+	"""
+	Implement print_nums_iterative above without using a loop.
+	"""
+	pass
+
+
+def print_nums_recursive_helper(current, n):
+	pass
+
+def find_char_a(string):
+	"""
+	Implement a function that returns the number of times the 
+	character "a" appears in a str without using a loop. You 
+	may not take a slice of the string, and you may only access 
+	a single character of the string in any iteration of the 
+	function.
+	"""
+	pass
+
+def find_char_a_helper(string, current_position):
+	pass
+
