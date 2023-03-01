cf6c680e9e22f2d049601f7340c8f1264d606b35
Sami Rollins <srollins@gmail.com>
2023-03-01 14:24:46 -0500

    updating naming

---
+++ inventory.py
@@ -0,0 +1,13 @@
+class Inventory:
+
+	def __init__(self):
+		pass
+
+	def add_new_item(self, new_item, number_available):
+		pass
+
+	def update_number_available(self, item_to_find, number_purchased):
+		pass
+
+	def show_inventory(self):
+		pass
---
+++ inventory_item.py
@@ -0,0 +1,15 @@
+class Inventory_Item:
+
+	# inventory_number_generator = 1
+
+	def __init__(self, name, quantity):
+		pass
+
+	def __str__(self):
+		pass
+
+	def purchase(self, num_to_purchase):
+		pass
+
+	def get_number_available(self):
+		pass
